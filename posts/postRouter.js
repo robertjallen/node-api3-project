2be01f9b30f9973788eed778d7505e80cd93d243
robertjallen <bobbidigi34@gmail.com>
2020-06-09 20:46:52 -0400

    add get post by ID

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,29 @@
 const express = require('express');
-
+const db = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get()
+  .then(posts =>{
+    if(posts){
+    return  res.status(200).json(posts)
+    }
+  })
+  .catch( err => {
+    res.status(500).json({message: "error retrieving posts"})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId(), (req, res) => {
+  db.getById(req.params.id)
+  .then(posts => {
+    if(posts){
+      return res.status(200).json(posts)
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: "Error retrieving post"})
+  })
 });
 
 router.delete('/:id', (req, res) => {
@@ -21,7 +37,20 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  return (req, res, next) => {
+		db.getById(req.params.id)
+			.then(post => {
+				if (post) {
+					req.post = post;
+					next();
+				} else {
+					res.status(404).json({ message: `Post with id ${req.params.id} does not exist` });
+				}
+			})
+			.catch(err => {
+				next(err);
+			});
+	};
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const server = express();
 const helmet = require('helmet');
 const userRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
 const colors = require('colors');
 
 server.use(logger);
 server.use(helmet());
 server.use(express.json())
 server.use('/api/users', userRouter);
+server.use('/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
