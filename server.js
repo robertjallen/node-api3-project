7233211bff3f9c4bac246ad5da6015f45ce82c0c
robertjallen <bobbidigi34@gmail.com>
2020-06-10 19:09:48 -0400

    fix cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
-var cors = require('cors')
+
 const userRouter = require('./users/userRouter');
 const postsRouter = require('./posts/postRouter');
 const colors = require('colors');
@@ -9,7 +9,7 @@ const colors = require('colors');
 server.use(logger);
 server.use(helmet());
 server.use(express.json())
-server.use(cors());
+
 server.use('/api/users', userRouter);
 server.use('/posts', postsRouter);
 
