8095e36a95d455194b5e25733ec1ff672c4bd551
robertjallen <bobbidigi34@gmail.com>
2020-06-10 17:01:47 -0400

    add localhost 4000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./server');
 var catMe = require('cat-me');
 var colors = require('colors');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(catMe('nyan').rainbow,` KATZ SERVING ON ${port}`)
