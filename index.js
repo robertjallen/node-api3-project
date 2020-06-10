b4279651d21f3c6071295ce4b80551031d1cc360
robertjallen <bobbidigi34@gmail.com>
2020-06-10 15:37:20 -0400

    add dynamic PORT for heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // code away!
+require('dotenv').config();
 const app = require('./server');
 var catMe = require('cat-me');
 var colors = require('colors');
 
-const PORT = 4000;
+const port = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(catMe('nyan').rainbow,` KATZ SERVING ON ${PORT}`)
+app.listen(port, () => {
+  console.log(catMe('nyan').rainbow,` KATZ SERVING ON ${port}`)
 });
