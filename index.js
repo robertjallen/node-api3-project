b8085d21b8e97eb9c90b70ed9753214e05112209
robertjallen <bobbidigi34@gmail.com>
2020-06-11 17:01:15 -0400

    add cors to server.js above all middleware

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // code away!
 require('dotenv').config();
-var cors = require('cors')
+
 const app = require('./server');
 var catMe = require('cat-me');
 var colors = require('colors');
 
-app.use(cors());
+
 
 const port = process.env.PORT || 4000;
 
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
-
+var cors = require('cors')
 const userRouter = require('./users/userRouter');
 const postsRouter = require('./posts/postRouter');
 const colors = require('colors');
 
+server.use(cors());
 server.use(logger);
 server.use(helmet());
 server.use(express.json())
